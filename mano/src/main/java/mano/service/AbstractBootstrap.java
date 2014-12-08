a43780dc096c4ed1a2fd70c6554dc2c557783a11
jun@diosay.com <jun@diosay.com>
2014-12-09 06:57:11 +0800

    服务生命周期调整

---
+++ mano/src/main/java/mano/service/AbstractBootstrap.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2014 The MANO Project. All rights reserved.
+ * 
+ * See more http://mano.diosay.com/
+ * 
+ */
+
+package mano.service;
+
+/**
+ * 表示服务容器启动程序。
+ * @author johnwhang
+ */
+public abstract class AbstractBootstrap {
+    
+}
